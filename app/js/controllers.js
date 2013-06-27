7bd4cf0ba9350c351fdd87128149ee10e7641ee4
Rob Dennis <rdennis@gmail.com>
2013-06-27 09:39:26 -0400

    more stubbing of ui elements

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,9 @@
 
 angular.module('lambic.controllers', []);
 
-function AppCtrl($scope, PoolService) {
+function AppCtrl($scope,
+                 $dialog,
+                 PoolService) {
     $scope.pool = PoolService.get();
     $scope.panes = [
         {name: 'White'},
@@ -16,4 +18,29 @@ function AppCtrl($scope, PoolService) {
         {name: 'Multicolor'},
         {name: 'Land'}
     ];
+
+    $scope.opts = {
+        backdrop: true,
+        keyboard: true,
+        backdropClick: true,
+        controller: 'AppCtrl'
+    };
+
+    $scope.importDialog = function() {
+        openDialog('<label>import dialog</label>')
+    };
+
+    $scope.exportDialog = function() {
+        openDialog('<label>export dialog</label>')
+    };
+
+    var openDialog = function(template){
+        var updatedOpts = angular.extend({template: template}, $scope.opts);
+        var d = $dialog.dialog(updatedOpts);
+        d.open().then(function(result){
+            if(result) {
+                alert('dialog closed with result: ' + result);
+            }
+        });
+  };
 }
