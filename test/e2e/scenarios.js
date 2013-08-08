b25f0355a72b39db75672aafefa755434248d8be
Rob Dennis <rdennis@gmail.com>
2013-08-07 21:40:19 -0400

    extremely basic e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,8 +4,17 @@
 
 describe('my app', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
-  });
+    beforeEach(function() {
+        browser().navigateTo('../../app/index.html');
+    });
+
+    describe('on load appearance', function() {
+        it('should have some number of of tabs', function() {
+            expect(repeater('.nav-tabs > li').count()).toBeGreaterThan(0);
+        });
+        it("there's one tab that is active", function() {
+            expect(repeater('.nav-tabs > li.active').count()).toEqual(1);
+        });
+    })
 
 });
