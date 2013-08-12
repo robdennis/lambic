5ad108d5432fc0cf1a571360efaf575d01707158
Rob Dennis <rdennis@gmail.com>
2013-08-11 23:15:10 -0400

    forgot to include the filter unit test

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -2,7 +2,33 @@
 
 /* jasmine specs for filters go here */
 
-describe('filter', function() {
-  beforeEach(module('lambic.filters'));
+describe('filter', function($filter) {
+    beforeEach(module('lambic.filters'));
 
+    describe('inSlicesOf', function() {
+
+        it('should pass some basic sanity checks',
+            inject(function(inSlicesOfFilter) {
+                var testArray = [0, 1, 2, 3, 4];
+                expect(inSlicesOfFilter(testArray, 0)).toEqual([
+                    [0], [1], [2], [3], [4]
+                ]);
+                expect(inSlicesOfFilter(testArray, 1)).toEqual([
+                    [0], [1], [2], [3], [4]
+                ]);
+                expect(inSlicesOfFilter(testArray, 2)).toEqual([
+                    [0, 1], [2, 3], [4]
+                ]);
+                expect(inSlicesOfFilter(testArray, 3)).toEqual([
+                    [0, 1, 2], [3, 4]
+                ]);
+                expect(inSlicesOfFilter(testArray, 4)).toEqual([
+                    [0, 1, 2, 3], [4]
+                ]);
+                expect(inSlicesOfFilter(testArray, 5)).toEqual([
+                    testArray
+                ]);
+            })
+        );
+    });
 });
